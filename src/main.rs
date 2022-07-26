e6a0e1af1e7934502af340128e80a9ea6a47792a
RinHizakura <s921975628@gmail.com>
2022-07-26 13:10:52 +0800

    Output file according to command args

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use crate::cat::CatRunner;
 use crate::err::*;
 use nix::fcntl::*;
 use std::fs::File;
-use std::io;
 use std::os::unix::io::AsRawFd;
+use std::{env, io, process};
 
 fn open_file(path: &str) -> Result<File> {
     let file = File::open(path)?;
@@ -27,7 +27,12 @@ fn cntl_stdout() -> Result<()> {
 }
 
 fn main() {
-    let file = open_file("src/main.rs").expect("Invalid path");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        process::exit(-1);
+    }
+
+    let file = open_file(&args[1]).expect("Invalid path");
     cntl_stdout().expect("cntl stdout fail");
 
     let mut cat_runner = CatRunner::new(file);
